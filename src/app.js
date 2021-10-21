cc74b2678e2b3b84a47a018f0d96610f908a60bb
cbell-smythe <50560779+cbell-smythe@users.noreply.github.com>
2021-10-21 18:47:52 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -47,8 +47,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(displayTemperature);
+}
 
-axios.get(`${apiUrl}`).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Paris");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
